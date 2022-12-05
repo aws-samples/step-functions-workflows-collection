229820009463b88ca8ed6c1eadf5615cc7ce928e
Mike Ellis <ellisms@amazon.com>
2022-12-05 16:17:36 -0500

    Formatted python files

--- batch-lambda-sam/src/function.py
+++ batch-lambda-sam/src/function.py
@@ -1,19 +1,12 @@
+import json
+import math
+import random
 
-import json 
-import math 
-import random 
-
-
-#generate a random int between 2 values
-def getRandomInt(min,max):
-    min = math.ceil(min)
-    max = math.floor(max)
-    return math.floor(random.random() * (max - min) + min )
 
 # Sample function with mocks the operation of checking the number of batch jobs needing fan out.
 # For demo purposes, it will return a random number between 2 and 10.
-def handler(event,context):
-    num = getRandomInt(2,10)
+def handler(event, context):
+    num = random.randint(2, 10)
 
-    result = {"input": str(num)} 
+    result = {"input": str(num)}
     return result
