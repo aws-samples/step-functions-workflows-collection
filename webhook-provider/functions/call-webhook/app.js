c74291dfd8dba66fff6526aeea073c2e0b62cacf
Subha Venugopal <110146290+subha-aws@users.noreply.github.com>
2023-04-30 02:37:47 -0400

    jj

--- webhook-provider/functions/call-webhook/app.js
+++ webhook-provider/functions/call-webhook/app.js
@@ -2,51 +2,61 @@ const https = require("https");
 
 module.exports.lambdaHandler = async (event) => {
   for (const message of event.Records) {
- 
-  const data = JSON.stringify({
-   'resource': message.resourceType,
-   'Event' : message.eventType,
-   'resourceId': message.resourceId,
-  });
-
-  let url = message.callbackURL;
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Content-Length": data.length,
-    },
-  };
-
-  let response = callapi(url, data, options);
-  console.log("Sent notification to customer");
-}
-return {
-  statusCode: 200,
-  body: JSON.stringify(
-    {
-      message: 'processed records',
-      input: event,
-    },
-    null,
-    2
-  ),
-};
-
-};
-
-let callapi = async (url, payload, options) =>
-  new Promise((resolve, reject) => {
-    let buffer = "";
-
-    const req = https.request(url, options, (res) => {
-      let buffer = "";
-      res.on("data", (chunk) => (buffer += chunk));
-
-      res.on("end", () => resolve(JSON.parse(buffer)));
+    const data = JSON.stringify({
+      resource: message.resourceType,
+      Event: message.eventType,
+      resourceId: message.resourceId,
+      createdAt: message.createdAt,
     });
 
-    req.on("error", (e) => reject(e.message));
-    req.write(payload);
+    //TODO: Decrypt and add the authz
+    let url = message.callbackURL;
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+      },
+    };
+
+    let response = callapi(url, data, options);
+    console.log("Sent notification to customer");
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "processed records",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+
+async function callapi(url, payload, options) {
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+
+      res.on("data", (d) => {
+        process.stdout.write(payload);
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(req);
+      console.error(error);
+    });
+
+    // set a timeout of 5 seconds
+    req.setTimeout(5000, () => {
+      req.destroy();
+    });
+
     req.end();
+    resolve(req);
   });
+}
--- webhook-provider/functions/prepare-webhook/app.js
+++ webhook-provider/functions/prepare-webhook/app.js
@@ -1,28 +1,54 @@
+ 
+const { createHmac } = require("crypto");
 const crypto = require("crypto");
 const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
 const dynamo = new DynamoDBClient(process.env.AWS_REGION);
+const docClient = DynamoDBDocumentClient.from(dynamo);
+const tableName = process.env.WEBHOOK_TABLE;
+const encoder = new TextEncoder();
 
-const tableName = process.env.DASHBOARD_TABLE_NAME;
+
 module.exports.lambdaHandler = async (event) => {
-   
-  
-  
+ console.log(event);
+  let callInfo = JSON.parse( JSON.stringify(event.webhookData.Item));
+  let currentTime = new Date().toUTCString();
+  let payLoad = event.detail;
+ console.log(callInfo);
+  console.log(payLoad);
+  let token=createHmac('sha256',encoder.encode(callInfo.signingToken.S)).update(encoder.encode(JSON.stringify(payLoad))).digest('hex');
+
+
+  let postData = {
+ 
+    pk: callInfo.pk.S + "+" + payLoad.id + "+" +crypto.randomUUID(),
+    type: "webhookcall",
+    url: callInfo.url.S,
+    payLoad:payLoad,
+    invokeTime: currentTime,
+    status: "pending",
+    token:token
+
+  };
+  console.log(postData);
   const params = {
     TableName: tableName,
-    Item: dataToWrite,
+    Item: postData,
   };
   console.log(params);
 
-  const docClient = DynamoDBDocumentClient.from(dynamo);
+
 
   try {
     const response = await docClient.send(new PutCommand(params));
     console.log(response);
+    return {
+      'id': postData.pk,
+      'webhookData': postData
+    };
   } catch (e) {
     console.error(e);
   }
  
-  };
-  
+};
