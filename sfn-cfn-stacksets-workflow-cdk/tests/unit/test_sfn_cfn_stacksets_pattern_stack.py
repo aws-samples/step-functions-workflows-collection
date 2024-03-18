878354cdedbd706e00a4dfd2b162d7bc0e588b0f
Singhal <ackshay@909c4ad31101.ant.amazon.com>
2024-03-18 13:45:54 -0700

    sfn-cfn-stacksets-workflow initial commit

---
+++ sfn-cfn-stacksets-workflow-cdk/tests/unit/test_sfn_cfn_stacksets_pattern_stack.py
@@ -0,0 +1,11 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from sfn_cfn_stacksets_pattern.sfn_cfn_stacksets_pattern_stack import SfnCfnStacksetsPatternStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in sfn_cfn_stacksets_pattern/sfn_cfn_stacksets_pattern_stack.py
+def test_cfn_stacksets_created():
+    app = core.App()
+    stack = SfnCfnStacksetsPatternStack(app, "sfn-cfn-stacksets-pattern")
+    template = assertions.Template.from_stack(stack)
